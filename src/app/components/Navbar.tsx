2eac3793155c8a3d1fe3b3aac146053855088d16
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-02-03 22:51:19 +0530

    Add sign-in page, navbar component, and utility functions; configure Tailwind CSS and global styles

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <nav className='flex justify-center'>
+        <ul>
+            <li>Home</li>
+            <li>About </li>
+            <li>Services</li>
+        </ul>
+    </nav>
+    
+  )
+}
+
+export default Navbar
